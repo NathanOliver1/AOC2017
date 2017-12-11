4e7f26196321c08ea6a90d4e1fb811241350685b
Nathan Oliver <nathanoliver60097@gmail.com>
2017-12-11 15:11:33 -0600

    Create Day4Part1.cpp

---
+++ Day4Part1.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <fstream>
+#include <vector>
+#include <sstream>
+#include <set>
+
+int main()
+{
+	std::string line;
+	int counter = 0;
+	std::ifstream fin("data.txt");
+	while (std::getline(fin, line))
+	{
+		int totalWords = 0;
+		std::set<std::string> data;
+		std::stringstream ss(line);
+		std::string temp;
+		while (ss >> temp)
+		{
+			data.insert(temp);
+			++totalWords;
+		}
+		if (totalWords == data.size())
+			++counter;
+	}
+	std::cout << counter;
+	return 0;
+}
